cc2b98c07a1e4dcc5ab38d7ac3ff20bd370a186b
Olukanni Oluwafunmilola deborah <funmilolaabiola002@gmail.com>
2021-07-08 12:59:03 +0100

    Changes to App.js

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,7 @@ export default function App() {
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Edit to see some magic happen!</h2>
-      <countbuttons increamentBy={2} bgColor={"blue"}/>
+      <countButtons increamentBy={2} bgColor={"blue"}/>
     </div>
   );
 }
