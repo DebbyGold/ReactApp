2bfd4af431b0090ba5d72a72bc419cc8065113d5
Olukanni Oluwafunmilola deborah <funmilolaabiola002@gmail.com>
2021-07-08 23:05:05 +0100

    Changes added to search bar file

--- src/components/searchBar/SearchBar.js
+++ src/components/searchBar/SearchBar.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React, {useState} from "react";
 import "./SearchBar.css";
 
 export default function SearchBar(){
 
+  const [searchValue, setSearchValue]=useState("search goes here");
+
+function handleClickInput(event){
+ setSearchValue(searchValue+event.target.value);
+}
+
   return(
      <div>
-       
+       <input type="text" value={searchValue} onChange={handleClickInput}/>
      </div>
   );
 }
