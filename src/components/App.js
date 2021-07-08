ab09b58a47f3fc573aa99a619cda763cf914619d
Olukanni Oluwafunmilola deborah <funmilolaabiola002@gmail.com>
2021-07-08 11:12:07 +0100

    Additions to app.js file

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import "../styles.css";
+import countButtons from "../components/countButtons";
 
 export default function App() {
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Edit to see some magic happen!</h2>
+      <countbuttons/>
     </div>
   );
 }
