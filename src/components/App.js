9e3ef90ad71c69ab7726903454402328ad0c84b7
Olukanni Oluwafunmilola deborah <funmilolaabiola002@gmail.com>
2021-07-11 16:48:00 +0100

    App file updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,37 @@
 import "../styles.css";
 import CountButtons from "../components/countButtons";
 import SearchBar from "../components/searchBar/SearchBar";
+import React, {useState,useEffect} from "react";
 
 
-export default function App() {
+export default function App(){
+
+const [productState, setProductState]=useState([]);
+
+useEffect(()=>{
+fetch('https://fakestoreapi.com/products')
+             .then(res=>res.json())
+                                      .then((productArray)=>{
+                                      const newProductState=productArray.map((product)=>{
+return product.title;
+                                      }
+                                      )
+setProductState(newProductState);
+                                    })
+},[])
+
   return (
     <div className="App">
       <h1>Counter</h1>
       <CountButtons increamentBy={2} bgColor={"blue"}/>
 <h2>Search items in thelists lists</h2>
-      <SearchBar products={[
-  "tooth paste",
-  "tooth brush",
-"dental flux",
-"mouth wash",
-"mouth guard"
-
-      ]}/>
+      <SearchBar products={productState}/>
+{/*
 <SearchBar products={[
 "motor bike",
 "power bike",
 "soccer ball"
-]}/>
+]}/>*/}
     </div>
   );
 }
